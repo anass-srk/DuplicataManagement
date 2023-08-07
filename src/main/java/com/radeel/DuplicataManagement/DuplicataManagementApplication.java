576e53e2f4e5f2dc69a9cd1e1cff2d25a4c940eb
Anass Serroukh <anasssrk1@gmail.com>
2023-08-07 22:51:33 +0100

    basic setup

---
+++ src/main/java/com/radeel/DuplicataManagement/DuplicataManagementApplication.java
@@ -0,0 +1,13 @@
+package com.radeel.DuplicataManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuplicataManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuplicataManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/radeel/DuplicataManagement/DuplicataManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radeel.DuplicataManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuplicataManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
